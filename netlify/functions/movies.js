7ffac3c76d33647d571b5a24b28f3d4151a1863d
Josh Rosenthal <josh.rosenthal@kellogg.northwestern.edu>
2021-05-09 14:00:39 -0500

    Final

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -21,7 +21,7 @@ exports.handler = async function(event) {
   // 🔥 hw6: your recipe and code starts here!
  
   let year = event.queryStringParameters.year
-  console.log(year)
+  // console.log(year)
   let genre = event.queryStringParameters.genre
 
   
@@ -31,21 +31,36 @@ exports.handler = async function(event) {
       body: `Nope!` // a string of data
     }
   }
+  // an else if statement that returns an error if a user does not submit a year and genre
+  else if (year.length < 1 || genre.length <1 ){
+    return{      
+    statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    body: `Error: Must Provide Year and Genre` // a string of data
+   }
+  }
+  //set up else statement that creates desired API
+  //Create empty list that will eventually receive number of results and list of movies
   else {
     let returnValue = {
       numResults: 0,
       movies:[]
     }
+  //create for loop that goes through the CSV doc
     for (let i=0; i < moviesFromCsv.length; i++) {
       let movie = moviesFromCsv[i]
+  //create conditional that removes any movie without a genre or run time
       if (movie.genres != `//N` || movie.runtimeMinutes != '//N') {
+  // create a conditional that forces user to input a genre and year   
       if (movie.genres == genre && movie.startYear == year){
-      let postObject = {
+  //make object containing information that is required
+        let postObject = {
         title: movie.originalTitle,
         releaseDate: movie.startYear,
         movieGenre: movie.genres
       }
+    //add object to empty movie list
       returnValue.movies.push(postObject)
+    //make number of results feed up to the object
       returnValue.numResults = returnValue.numResults + 1
       }
     }
@@ -54,6 +69,7 @@ exports.handler = async function(event) {
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    //output is stringed version of the return value above
       body: JSON.stringify(returnValue) // a string of data
     }
   }
